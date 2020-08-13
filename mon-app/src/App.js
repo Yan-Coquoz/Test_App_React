7d97015b2832f8845a1ede65dffbaaab66ea3dbf
Yan-Coquoz <Yan.Coquoz@gmail.com>
2020-08-13 18:31:28 +0200

    module mike 10c

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { PureCompo, PureCompoProps } from "./componants/PureComp.js";
 import StateCompo from "./componants/StateComp";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <PureCompo />
+class App extends Component {
+  onInputChange(val) {
+    console.log("val :" + val);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <PureCompo />
 
-      <PureCompoProps label="Yan" age="43"/>
+          <PureCompoProps label="Yan" age="43" />
 
-      <StateCompo name={"Yan"}></StateCompo>
+          <StateCompo
+            label={"Yan"}
+            onInputChange={this.onInputChange}
+          ></StateCompo>
 
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        
-      </header>
-    </div>
-  );
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
- // module 10b 27min pures compo et compo avec state
+
+// module 10c 16 min 
--- mon-app/src/componants/StateComp.js
+++ mon-app/src/componants/StateComp.js
@@ -1,21 +1,40 @@
 /**
  * @author YC
- * 
+ * Les States
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./StateComp.css";
 
-class StateCompo extends Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return(
-            <div>
-                <h2 className="state-h2">Compo State</h2>
-            </div>
-        )
-    }
+class StateCompo extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Mon label", props.label);
+    this.state = { nom: "" };
+    //this.handleChange = this.handleChange.bind(this);
+    // bind dans le constructeur est préfairable. ou le faire en F=> sans le bind dans le constructeur
+  }
+  // handleChange(e){
+  //     console.log("Je passe par ici");
+  //     this.setState({nom:"Le nouveau nom"})
+  // }
+  handleChange = (e) => {
+    console.log("la valeur de e : ", e.target.value);
+    this.setState({nom: e.target.value});
+    this.props.onInputChange(e.target.value);
+  };
+
+  render() {
+    return (
+      <div>
+        <h2 className="state-h2">Compo State {this.props.label}</h2>
+        <input
+          type="text"
+          value={this.state.nom}
+          onChange={this.handleChange}
+        ></input>
+      </div>
+    );
+  }
 }
 export default StateCompo;
